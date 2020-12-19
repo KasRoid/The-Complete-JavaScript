18130c4ddb0bfe7463247ec0a8e570e2960a77ec
KasRoid <KasRoid@gmail.com>
2020-12-19 10:23:58 +0900

    feat: Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,15 @@
-let js = 'amazing';
-if (js == 'amazing') alert('JavaScript is JUN!');
-
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
