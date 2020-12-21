2a7aedfce4587a9dcd484558ec63bf7b24191855
KasRoid <KasRoid@gmail.com>
2020-12-22 07:16:21 +0900

    feat: Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,6 +4,7 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
+// Variable name conventions
 let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
@@ -13,3 +14,23 @@ let PI = 3.1415;
 
 let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Song");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1988;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
