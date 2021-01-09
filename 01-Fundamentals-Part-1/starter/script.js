cb9ea72c400d0ab74d7eaa74d65f27ca56b3be2f
KasRoid <KasRoid@gmail.com>
2021-01-09 22:15:57 +0900

    feat: Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,26 @@
+// Type Conversion and Coercion
+// Type Conversion
+const inputYear = `1991`;
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18);
+
+console.log(Number(`Kas`)); // NaN
+console.log(typeof NaN);
+
+console.log(String(23));
+
+// Type Coercion
+console.log("I am " + 23 + " years old");
+console.log("23" + "10" + 3);
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+console.log("23" > "18");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n)
+
+/*
 // Coding Challenge 2
 const marksBMI = 28.3;
 const johnsBMI = 23.9;
@@ -10,7 +33,6 @@ if (marksBMI > johnsBMI) {
     console.log(`John's BMI (${johnsBMI}) is higher than Mark's (${marksBMI})`)
 }
 
-/*
 // Taking Decisions: If/Else Statements
 const age = 15;
 const isOldEnough = age >= 18;
