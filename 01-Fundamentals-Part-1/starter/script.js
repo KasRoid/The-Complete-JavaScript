41b9f1d7843ec938ec8a53b7244d7e47a9c2dab9
KasRoid <KasRoid@gmail.com>
2021-01-15 22:31:14 +0900

    feat: The switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,47 @@
+// The switch statement
+
+const day = 'monday';
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    // break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'satuday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
+
+
+/*
 // Coding Challenge 3
 let dolphins1 = 96;
 let dolphins2 = 108;
@@ -53,7 +97,6 @@ if (dolphinsAverage > koalasAverage && dolphinsAverage > 100) {
 
 console.log(`Dolphin: ${dolphinsAverage}, Koalas: ${koalasAverage}`);
 
-/*
 // Logical Operators
 const hasDriversLicense = true; // A
 const hasGoodVision = false; // B
