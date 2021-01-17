58afac333aee7b0657e6f0d6cc6f9308489f7631
KasRoid <KasRoid@gmail.com>
2021-01-17 18:41:40 +0900

    feat: Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
+// Functions
+function logger() {
+    console.log('My name is Kas');
+}
+
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+/*
 // Strict Mode
 let hasDriversLicense = false;
 const passTest = true;
@@ -10,3 +31,4 @@ if (hasDriversLicense) console.log('I can drive :D');
 // const interface = 'Audio';
 // const private = 534;
 // const if = 23;
+*/
