cd07ff744cc1e2b6dfb841636b5d91c649dc69a7
KasRoid <KasRoid@gmail.com>
2021-02-02 11:05:10 +0900

    feat: Find Max and Min, Filtering by types, Concat arrays

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,44 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+// Find Max and Min, Filtering by types, Concat arrays
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = (temps) => {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = (t1, t2) => {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+/*
 // Setting up Prettier
 const x = 23;
 if (x === 23) console.log(23);
+*/
