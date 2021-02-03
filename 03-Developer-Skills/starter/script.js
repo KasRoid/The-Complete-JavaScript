b38c4b94dc3066916bbf1189dd1b587e66a8c5a6
KasRoid <KasRoid@gmail.com>
2021-02-03 09:17:49 +0900

    feat: Debugging with the Console and Breakpoints

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,48 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+// Debugging with the Console and Breakpoints
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: 10,
+    // value: Number(prompt("Degrees celsius:")),
+  };
+
+  console.table(measurement);
+  console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = (t1, t2) => {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    debugger;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+
+/*
 // Find Max and Min, Filtering by types, Concat arrays
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
@@ -37,7 +79,6 @@ const calcTempAmplitudeNew = (t1, t2) => {
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
 
-/*
 // Setting up Prettier
 const x = 23;
 if (x === 23) console.log(23);
