d6bc8d542f5905ef4e7a2acae473b87521c74912
KasRoid <KasRoid@gmail.com>
2021-02-07 09:47:28 +0900

    feat: Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+// DOM(Documnet Object Model) - Structured representation of HTML Documents. Allows JavaScript to access HTML elements and styles to manipulated them.
+
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '1';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
