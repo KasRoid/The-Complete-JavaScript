00dcbff0871ee4c19fb21d82b66199cc9090923b
KasRoid <KasRoid@gmail.com>
2021-02-07 10:25:39 +0900

    feat: Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 3;
+document.querySelector('score').textContent = score;
+let gameCleared = false;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    gameCleared = true;
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber && score > 0) {
+    document.querySelector('.message').textContent = 'Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber && score > 0) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+
+  if (score === 0 && !gameCleared) {
+    document.querySelector('.message').textContent = 'You lost the game!';
   }
 });
 
