0222aca0211033530acac236f4ed7adb304fd781
KasRoid <KasRoid@gmail.com>
2021-02-11 01:57:47 +0900

    feat: Modal Window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const openModalButtons = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < openModalButtons.length; i++) {
+  console.log(openModalButtons[i].textContent);
+}
