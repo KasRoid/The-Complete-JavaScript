65667bb678184948ae11cbb0b245fbdb02559787
KasRoid <KasRoid@gmail.com>
2021-02-16 09:19:04 +0900

    feat: Project #3: Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// Score Elements
+const score1El = document.querySelector("#score--0");
+const score2El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+// Start Conditions
+score1El.textContent = "0";
+score2El.textContent = "0";
+diceEl.classList.add("hidden");
