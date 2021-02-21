ca75a28adbdc65523b5f606619998d0899539036
KasRoid <KasRoid@gmail.com>
2021-02-21 10:45:22 +0900

    feat: Switching the Active Player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Score Elements
+const player0E = document.querySelector(".player--0");
+const player1E = document.querySelector(".player--1");
 const score1El = document.querySelector("#score--0");
 const score2El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -10,7 +12,9 @@ const newButton = document.querySelector(".btn--new");
 const rollButton = document.querySelector(".btn--roll");
 const holdButton = document.querySelector(".btn--hold");
 
+let scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 const didClickRollButton = () => {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -18,8 +22,15 @@ const didClickRollButton = () => {
   diceEl.src = `dice-${dice}.png`;
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0E.classList.toggle("player--active");
+    player1E.classList.toggle("player--active");
   }
 };
 
