1d991617c71c9c39951285a5ed4f3f6961826104
KasRoid <KasRoid@gmail.com>
2021-02-28 20:20:25 +0900

    feat: Scoping in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+
+  const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+  console.log(output);
+
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    const firstName = "Steven";
+    const str = `Oh, and you're a millenial, ${firstName}`;
+    console.log(str);
+  }
+  return age;
+}
+
+const firstName = "Kas";
+calcAge(1988);
