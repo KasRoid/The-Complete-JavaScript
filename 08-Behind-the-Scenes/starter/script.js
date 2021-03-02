d752bcbe3a4a2f84c3cd4ceb03462249924af507
KasRoid <KasRoid@gmail.com>
2021-03-03 07:09:19 +0900

    feat: Regular Functions vs. Arrow Functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,4 +1,31 @@
 "use strict";
+console.clear();
+
+// Regular Functions vs. Arrow Functions
+const myself = {
+  fistName: "kas",
+  year: 1988,
+  calcAge: function () {
+    console.log("myself: ", 2021 - this.year);
+
+    const self = this;
+    const isMillenial = function () {
+      console.log("myself isMillenial this: ", this);
+      console.log("myself isMillenial self: ", self);
+    };
+    const isMillenialArrow = () => {
+      console.log("myself isMillenialArrow this: ", this);
+    };
+    isMillenial();
+    isMillenialArrow();
+  },
+  greet: () => {
+    console.log("myself greet this: ", this);
+  },
+};
+
+myself.calcAge();
+myself.greet();
 
 // The this Keyword in Practice
 console.log("this: ", this);
@@ -11,8 +38,8 @@ const calculateArrow = number => {
   console.log(100 - number);
   console.log("this in calculateArrow: ", this);
 };
-calculate(10);
-calculateArrow(10);
+// calculate(10);
+// calculateArrow(10);
 
 const kas = {
   year: 1988,
@@ -20,19 +47,19 @@ const kas = {
     console.log("this in kas: ", this);
   },
 };
-kas.calcAge();
+// kas.calcAge();
 
 const matilda = {
   year: 1993,
 };
 matilda.calcAge = kas.calcAge;
-matilda.calcAge();
+// matilda.calcAge();
 
 const f = kas.calcAge;
-f();
+// f();
 
 // Hoisting and TDZ Practice
-console.log(me);
+// console.log(me);
 // console.log(teacher);
 // console.log(year);
 
@@ -40,7 +67,7 @@ var me = "Kas";
 let job = "iOS Developer";
 const year = 1988;
 
-console.log(addDecl(2, 3));
+// console.log(addDecl(2, 3));
 // console.log(addEConst(2, 3));
 // console.log(addArror(2, 3));
 
@@ -53,7 +80,7 @@ const addEConst = function addDecl(a, b) {
 var addArror = (a, b) => a + b;
 
 // Example of Hoisting
-if (!numberOfProducts) deleteShoppingCart();
+// if (!numberOfProducts) deleteShoppingCart();
 var numberOfProducts = 10;
 
 function deleteShoppingCart() {
@@ -64,9 +91,9 @@ var x = 1;
 let y = 2;
 const z = 3;
 
-console.log(x === window.x);
-console.log(x === window.y);
-console.log(x === window.z);
+// console.log(x === window.x);
+// console.log(x === window.y);
+// console.log(x === window.z);
 
 // Scoping in Practice
 function calcAge(birthYear) {
@@ -84,4 +111,4 @@ function calcAge(birthYear) {
 }
 
 const firstName = "Kas";
-calcAge(1988);
+// calcAge(1988);
