31ae43b8dd3bfdbdfd6222a2e5b0c0c690099f92
KasRoid <KasRoid@gmail.com>
2021-03-05 21:40:51 +0900

    feat: Primitives vs. Objects in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,39 @@
 "use strict";
 console.clear();
 
+// Primitives vs. Objects in Practice
+let lastName = "Song";
+let oldLastName = lastName;
+lastName = "Davis";
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = "Davis";
+// console.log("Before marriage", jessica);
+// console.log("After marriage", marriedJessica);
+
+const jessica2 = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alice", "Bob"],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = "Davis";
+
+jessicaCopy.family.push("Mary");
+jessicaCopy.family.push("John");
+
+console.log("Before marriage", jessica2);
+console.log("After marriage", jessicaCopy);
+
 // Primitives vs. Objects
 let age = 30;
 let oldAge = age;
@@ -14,8 +47,8 @@ const i = {
 };
 const friend = i;
 friend.age = 27;
-console.log("I: ", i);
-console.log("Friend: ", friend);
+// console.log("I: ", i);
+// console.log("Friend: ", friend);
 
 // Regular Functions vs. Arrow Functions
 const myself = {
