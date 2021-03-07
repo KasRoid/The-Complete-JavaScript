88dade4b57329bd833727e724cbcfd806f34003b
KasRoid <KasRoid@gmail.com>
2021-03-07 15:01:30 +0900

    feat: Destructuring Objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,7 +26,56 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${time} at ${address}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: "Goodnight Way. Apt 2",
+  starterIndex: 1,
+});
+
+// Destructing Object
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant; // Defualt Values
+console.log(menu, starters);
+
+let g = 111; // Mutating Variables
+let h = 999;
+const obj = { g: 23, h: 7, i: 14 };
+({ g, h } = obj);
+console.log(g, h);
+
+const {
+  fri: { open: openHour, close: closeHour },
+} = openingHours; // Nested Objects
+console.log(openHour, closeHour);
 
 // Destructuring Arrays
 const arr = [2, 3, 4];
@@ -35,7 +84,7 @@ const b = arr[0];
 const c = arr[0];
 
 const [x, y, z] = arr;
-console.log(x, y, z);
+// console.log(x, y, z);
 
 let [main, , secondary] = restaurant.categories;
 
@@ -44,15 +93,15 @@ let [main, , secondary] = restaurant.categories;
 // secondary = temp;
 
 [main, secondary] = [secondary, main]; // Switching Varibales
-console.log(main, secondary);
+// console.log(main, secondary);
 
 const [starter, mainCourse] = restaurant.order(2, 0); // Receive 2 return values from a function
-console.log(starter, mainCourse);
+// console.log(starter, mainCourse);
 
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 const [i, , [j, k]] = nested; // Nested Destructuring
-console.log(i, j, k);
+// console.log(i, j, k);
 
 const [p = 1, q = 1, r = 1] = [8, 9]; // Defualt Values
-console.log(p, q, r);
+// console.log(p, q, r);
