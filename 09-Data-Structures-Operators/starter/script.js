318fe0199e8df4d2e5b935431779bacf8585514e
KasRoid <KasRoid@gmail.com>
2021-03-12 22:50:40 +0900

    feat: Coding Challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,7 +57,76 @@ const nullishCoalescingConosoleLog = () => {
   console.log(guestCorrect);
 };
 
-nullishCoalescingConosoleLog();
+// nullishCoalescingConosoleLog();
+
+// Coding Challenge #1
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = game.players[0];
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+const printGoals = (...players) => {
+  console.log(`${players.length} goals were scored`);
+};
+
+const challenge1ConsoleLog = () => {
+  console.log(gk, fieldPlayers);
+  console.log(allPlayers);
+  console.log(players1Final);
+  console.log(team1, draw, team2);
+  printGoals(...game.scored);
+  team1 < team2 && console.log("Team 1 is more likely to win");
+  team1 > team2 && console.log("Team 2 is more likely to win");
+};
+
+challenge1ConsoleLog();
 
 // Short Circuiting
 restaurant.numGuests = 23;
