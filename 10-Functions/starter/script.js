346ea061d5e2c29b5d60af2fad43dffe5b351ccd
KasRoid <KasRoid@gmail.com>
2021-04-17 13:58:55 +0900

    feat: Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+console.clear();
+
+defaultParameters();
+
+function defaultParameters() {
+  const bookings = [];
+  const createBooking = (flightNumber, numberOfPassengers = 1, price = 199) => {
+    const booking = {
+      flightNumber,
+      numberOfPassengers,
+      price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+  };
+  createBooking('LH123');
+  createBooking('LH123', '2', '800');
+}
