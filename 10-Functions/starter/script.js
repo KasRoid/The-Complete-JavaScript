5ae3862f95a39d1fef6ae79b3f674fedd0024736
KasRoid <KasRoid@gmail.com>
2021-04-18 13:31:51 +0900

    feat: Passing Arguments - Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,7 +1,37 @@
 'use strict';
 console.clear();
 
-defaultParameters();
+passingArguments();
+// defaultParameters();
+
+function passingArguments() {
+  const flight = 'LH234';
+  const kas = {
+    name: 'Kas Song',
+    passport: 23242523523,
+  };
+  const checkIn = (flightNumber, passenger) => {
+    flightNumber = 'LH999';
+    passenger.name = 'Mr. ' + passenger;
+    if (passenger.passport === 23242523523) {
+      console.log('Checked in');
+    } else {
+      console.log('Wrong passport!');
+    }
+  };
+  checkIn(flight, kas);
+  console.log(flight);
+  console.log(kas);
+
+  const flightNumber = flight;
+  const passenger = kas;
+
+  const newPassport = person => {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+  };
+  newPassport(kas);
+  checkIn(flight, kas);
+}
 
 function defaultParameters() {
   const bookings = [];
